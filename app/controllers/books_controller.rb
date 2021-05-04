f45db5cdeccb469f3b559169827e6e2d925386c7
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-04 10:19:08 +0000

    課題中断

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
   def index
+    @lists = Book.all
+    @list = Book.new
   end
 
   def show
   end
 
   def new
+    @list = Book.all
+  end
+
+  def create
+    list = Book.new(book_params)
+    list.save
+    redirect_to books_path
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end

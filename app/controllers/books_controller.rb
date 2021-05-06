2c5e413c299017b5bd3d81fe43f4e3f0697b47b7
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-06 10:19:29 +0000

    課題中断

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
   def index
-    @lists = Book.all
-    @list = Book.new
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
-    @list = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -13,30 +13,34 @@ class BooksController < ApplicationController
   end
 
   def create
-    list = Book.new(book_params)
-    if list.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(list.id)
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
 
   def edit
-    @list = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def update
-    list = Book.find(params[:id])
-    if list.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(list.id)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.find(params[:id])
+      render :edit
     end
   end
 
   def destroy
-    list = Book.find(params[:id])
-    list.destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
